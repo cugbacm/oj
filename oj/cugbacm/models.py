0bc116fbfd37a7b00cbeb46adbb16e4ad598368d
cugbacm <acmcugb@gmail.com>
2014-03-11 20:43:44 +0800

    test

--- oj/cugbacm/models.py
+++ oj/cugbacm/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	name = models.CharField(max_length = 200)
+	password = models.CharField(max_length = 20)
+	session = models.CharField(max_length = 20)
+	specialty = models.CharField(max_length = 100)
+	tel = models.CharField(max_length = 100)
+	email = models.EmailField(max_length = 100)
+	nickname = models.CharField(max_length = 100)
+	def __unicode__(self):
+		return self.name
+
+class Submit(models.Model):
+	runID = models.IntegerField()
+	userID = models.CharField(max_length = 100)
+	problemID = models.IntegerField()
+	result = models.CharField(max_length = 100)
+	memory = models.CharField(max_length = 100)
+	time = models.CharField(max_length = 100)
+	codeLength = models.CharField(max_length = 100)
+	timestamp = models.TimeField()
+	def __unicode__(self):
+		return self.runID		
--- oj/cugbacm/views.py
+++ oj/cugbacm/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from cugbacm import views
 # Create your views here.
+
+urlpatterns = patterns('',
+	url(r'^index/$', views.index, name = 'index'),
+)
