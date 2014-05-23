1418ac35f85412961cf487364205b40f3db52eab
root <root@cugbacm-Lenovo-Product.(none)>
2014-05-23 13:54:30 +0800

    search

--- oj/cugbacm/views.py
+++ oj/cugbacm/views.py
@@ -283,7 +283,32 @@ def problemList(request):
 	try:
 		user = User.objects.get(userID = request.session['userID'])
 		problems = Problem.objects.all()
-		return render(request, 'cugbacm/problemList.html', {'problems': problems, 'userID':request.session["userID"]})
+		if request.method == 'POST':
+			problemID = request.POST['problemID']
+			problemTitle = request.POST['problemTitle']
+			problemAuthor = request.POST['problemAuthor']
+			try:
+				if problemID.strip():
+					problems = problems.filter(problemID = problemID)
+				if problemTitle.strip():
+					problems = problems.filter(title = problemTitle)
+				if problemAuthor.strip():
+					problems = problems.filter(author = problemAuthor)
+				return render(
+					request, 
+					'cugbacm/problemList.html', 
+					{
+						'problems': problems, 
+						'userID': request.session["userID"],
+						'problemID': problemID,
+						'problemTitle': problemTitle,
+						'problemAuthor': problemAuthor
+					}
+				)
+			except:
+				return render(request, 'cugbacm/problemList.html', {'problems': {}, 'userID':request.session["userID"]})
+		else:
+			return render(request, 'cugbacm/problemList.html', {'problems': problems, 'userID':request.session["userID"]})
 	except:
 		return HttpResponseRedirect("/index/login")
 
