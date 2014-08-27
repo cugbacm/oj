96fb63dcde95ec97f2d469ee70d533eabae9d2a4
git <763687347@qq.com>
2014-08-28 00:37:25 +0800

    url.py

--- oj/cugbacm/urls.py
+++ oj/cugbacm/urls.py
@@ -1,22 +1,28 @@
 from django.conf.urls import patterns, url
-from cugbacm import views
+import views.register
+import views.contest
+import views.contest_submit_list
+import views.contest_rank_list
+import views.login
+import views.problem_list
+import views.contest_list
+import views.submit_list
+import views.user_list
+import views.problem
+import views.user_info
+import views.contest_problem
 
 urlpatterns = patterns('',
-  url(r'^register/$', views.register, name = 'register'),
-  url(r'^problem/(?P<problem_id>\d+)/submit$', views.submit, name = 'submit'),
-  url(r'^contest/(?P<contest_id>\d+)$', views.contest, name = 'contest'),
-  url(r'^contest/(?P<contest_id>\d+)/submitList$', views.contestSubmitList, name = 'contestSubmitList'),
-  url(r'^contest/(?P<contest_id>\d+)/rankList$', views.contestRankList, name = 'contestRankList'),
-  url(r'^addProblem/$', views.addProblem, name = 'addProblem'),
-  url(r'^login/$', views.login, name = 'login'),
-  url(r'^problemList/$', views.problemList, name = 'problemList'),
-  url(r'^contestList/$', views.contestList, name = 'contestList'),
-  url(r'^testdata/$', views.testdata, name = 'testdata'),
-  url(r'^submitList/$', views.submitList, name = 'submitList'),
-  url(r'^userList/$', views.userList, name = 'userList'),
-  url(r'^gettest/$', views.gettest, name = 'gettest'),
-  url(r'^problem/(?P<problem_id>\d+)$', views.problem, name = 'problem'),
-  url(r'^showCode/(?P<submit_id>\d+)$', views.showCode, name = 'showCode'),
-  url(r'^userInfo/(?P<user_id>\w+)$', views.userInfo, name = 'userInfo'),
-  url(r'^contest/(?P<contest_id>\d+)/problem/(?P<problem_id>\d+)$',views.contestProblem, name = 'contestProblem'),
+  url(r'^register/$', views.register.register, name = 'register'),
+  url(r'^contest/(?P<contest_id>\d+)$', views.contest.contest, name = 'contest'),
+  url(r'^contest/(?P<contest_id>\d+)/submitList$', views.contest_submit_list.contestSubmitList, name = 'contestSubmitList'),
+  url(r'^contest/(?P<contest_id>\d+)/rankList$', views.contest_rank_list.contestRankList, name = 'contestRankList'),
+  url(r'^login/$', views.login.login, name = 'login'),
+  url(r'^problemList/$', views.problem_list.problemList, name = 'problemList'),
+  url(r'^contestList/$', views.contest_list.contestList, name = 'contestList'),
+  url(r'^submitList/$', views.submit_list.submitList, name = 'submitList'),
+  url(r'^userList/$', views.user_list.userList, name = 'userList'),
+  url(r'^problem/(?P<problem_id>\d+)$', views.problem.problem, name = 'problem'),
+  url(r'^userInfo/(?P<user_id>\w+)$', views.user_info.userInfo, name = 'userInfo'),
+  url(r'^contest/(?P<contest_id>\d+)/problem/(?P<problem_id>\d+)$', views.contest_problem.contestProblem, name = 'contestProblem'),
   )
