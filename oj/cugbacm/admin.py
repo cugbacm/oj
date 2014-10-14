6079b235a670cd957d3545e78101af75e399da77
akfheaven <2454147892@qq.com>
2014-10-14 21:28:31 +0800

    updat

---
+++ oj/cugbacm/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from cugbacm.models import User, Submit, Problem, Contest, ContestSubmit,Contestant
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Submit)
+admin.site.register(Problem)
+admin.site.register(Contest)
+admin.site.register(ContestSubmit)
+admin.site.register(Contestant)
