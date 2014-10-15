056dd70485e0637379a498622cec045cb64a328b
git <763687347@qq.com>
2014-10-15 15:03:54 +0800

    ..

---
+++ oj/cugbacm/views/ssdb_api.py
@@ -0,0 +1,31 @@
+from ssdb import SSDB
+import sys
+sys.path.append("..")
+import proto.rank_pb2
+
+ssdb_ip = "127.0.0.1"
+ssdb_port = "6666"
+ssdb = SSDB(host=ssdb_ip, port=ssdb_port)
+
+def GetContestRankListProto(contestID):
+  global ssdb
+  proto_str = ""
+  try:
+    proto_str = ssd.get(contestID)
+  except:
+    pass
+  return proto_str
+
+def SetContestRankListProto(contestID, rank_list_proto_str):
+  global ssdb
+  try:
+    ssdb.set(contestID, rank_list_proto_str)
+  except:
+    pass
+
+if __name__ == '__main__':
+  ssdb = SSDB(host='127.0.0.1', port=6666)
+  ssdb.set("test", "ttt")
+  print ssdb.get("test")
+
+
