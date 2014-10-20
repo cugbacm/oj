b44c9a52a5610c1135290c7e8497d2a6f4c98e2a
zhaolong <zhaolong@ubuntu.ubuntu-domain>
2014-10-20 20:09:40 +0800

    zl1020

--- oj/cugbacm/views/problem.py
+++ oj/cugbacm/views/problem.py
@@ -33,10 +33,10 @@ def Judge(submit):
   if submit.status == "Accepted":
     if Submit.objects.filter(userID = user.userID, problemID = submit.problemID).count() == 1:
       user.accepted = user.accepted + 1
-    if user.acList == None:
-      user.acList = ""
+      if user.acList == None:
+        user.acList = ""
    # user.accepted = user.accepted + 1
-    user.acList += str(submit.problemID) + ","
+      user.acList += str(submit.problemID) + ","
     problem.ac = problem.ac + 1
   elif submit.status == "Time Limit Exceeded":
     problem.tle = problem.tle + 1
