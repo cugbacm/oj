a0cc6ebb593f29c841b6043649684bc2897f8115
cugbacm <763687347@qq.com>
2015-12-25 14:31:04 +0800

    views

--- oj/cugbacm/views.py
+++ oj/cugbacm/views.py
@@ -1,17 +1,26 @@
 # coding=utf-8
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.views.generic import View, DetailView
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
+from .models import Problem
+from .models import Submit
 
 # Create your views here.
 # 题目详情
 class ProblemView(View):
+    template_name = 'cugbacm/problem.html'
+
     def get(self, request, problem_id):
-        return HttpResponse('problem view get ' + str(problem_id))
+        try:
+            problem = Problem.objects.get(problem_id=problem_id)
+            return render_to_response(ProblemView.template_name, {'problem': problem})
+        except:
+            return HttpResponse("problem does not exist " + str(problem_id))
 
     def post(self, request, problem_id):
         return HttpResponse('problem view post' + str(problem_id))
@@ -19,8 +28,21 @@ class ProblemView(View):
 # 登陆界面
 class LoginView(View):
     template_name = 'cugbacm/login.html'
+
+    def go_to_next(self, request):
+        # 默认跳转到problem_list页面
+        next = '/problem_list'
+        if request.GET.get('next'):
+            # 如果有指定的跳转页，则跳转到该页面
+            next = request.GET.get('next')
+        return HttpResponseRedirect(next)
+
     def get(self, request):
-        return render_to_response(LoginView.template_name)
+        # 如果用户已经登录就跳转
+        if request.user.is_authenticated():
+            return self.go_to_next(request)
+        else:
+            return render_to_response(LoginView.template_name, {'full_path': request.get_full_path()})
 
     def post(self, request):
         username = request.POST['username']
@@ -29,11 +51,8 @@ class LoginView(View):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return HttpResponse("login!")
-            else:
-                return HttpResponse("fuck")
-        else:
-            return HttpResponse("fuck")
+                return self.go_to_next(request)
+        return HttpResponseRedirect('/accounts/login')
 
 # 题目列表
 class ProblemListView(View):
--- oj/oj/wsgi.py
+++ oj/oj/wsgi.py
@@ -4,11 +4,13 @@ WSGI config for oj project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oj.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oj.settings")
+
 application = get_wsgi_application()
